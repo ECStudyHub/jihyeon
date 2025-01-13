4d40193694bce8ed033138eb84f35ce0b6f2f67a
Dam <60846068+j2h30728@users.noreply.github.com>
2025-01-13 23:27:43 +0900

    chore: 코드 수정

--- 1week_photocat/src/components/Loading.js
+++ 1week_photocat/src/components/Loading.js
@@ -2,7 +2,8 @@ export default class Loading {
   constructor({ $target, state }) {
     const template = `
         <div class='loading'>
-            <div>loading... </ div>
+            <div>loading... </div>
+            <button class="reload">새로고침</button>
         </div>`;
     $target.innerHTML += template;
     this.state = state;
@@ -16,6 +17,11 @@ export default class Loading {
 
   render() {
     const $loading = document.querySelector(".loading");
+    const $reload = document.querySelector(".reload");
+
+    $reload.addEventListener("click", () => {
+      window.location.reload();
+    });
 
     if (this.state) {
       $loading.style.display = "block";
